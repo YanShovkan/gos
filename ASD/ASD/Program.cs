3ca9f5fdd020169d219b0993b07f89a1d9a2b223
Yan <Yan@DESKTOP-GDBDHLQ>
2023-05-18 21:30:25 +0400

    add sorts

---
+++ ASD/ASD/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ASD;
+
+Console.WriteLine("Hello, World!");
+
+Sorts sorts = new Sorts();
+var array = sorts.FillArray(20);
+sorts.PrintArray(array);
+
+sorts.GnomeSort(array);
+
+sorts.PrintArray(array);
