12f3419377ff1f9a5aa8a53da6e6b510be1cedeb
Yan <Yan@DESKTOP-GDBDHLQ>
2023-05-20 23:14:47 +0400

    хеш таблица

--- ASD/ASD/Program.cs
+++ ASD/ASD/Program.cs
@@ -1,12 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-using ASD;
+﻿using ASD;
 
-Console.WriteLine("Hello, World!");
+//Sorts sorts = new Sorts();
+//var array = sorts.FillArray(20);
+//sorts.PrintArray(array);
 
-Sorts sorts = new Sorts();
-var array = sorts.FillArray(20);
-sorts.PrintArray(array);
+//sorts.GnomeSort(array);
 
-sorts.GnomeSort(array);
+//sorts.PrintArray(array);
 
-sorts.PrintArray(array);
+var hashTable = new HashTableChainMethod(10);
+for(int i = 1; i < 20; i++)
+{
+    hashTable.Insert(new PersonModel { Age = i, Name = "Ян" });
+}
+
+hashTable.Delete(new PersonModel { Age = 3, Name = "Ян" });
+hashTable.Insert(new PersonModel { Age = 55, Name = "Ян" });
+hashTable.Print();
+
+
+for (int i = 1; i < 20; i++)
+{
+    var model = new PersonModel { Age = i, Name = "Ян" };
+    Console.WriteLine($"Поиск элемента {model.Name} - {model.Age} : {hashTable.Find(model)}");
+}
+
