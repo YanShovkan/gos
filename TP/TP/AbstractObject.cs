f7a3da1d931484757b71482ef0d5091c4916294e
Yan <Yan@DESKTOP-GDBDHLQ>
2023-05-20 17:53:02 +0400

    эгов тп

---
+++ TP/TP/AbstractObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Serialization;
+
+namespace TP;
+
+[XmlRoot("elem")]
+public class AbstractObject
+{
+    [XmlElement("digit")]
+    public int AbstractInt { get; set; }
+
+    [XmlElement("string")]
+    public string AbstractString { get; set; }
+
+    [XmlArray("list")]
+    public List<object> AbstractList { get; set; }
+}
---
+++ TP/TP/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TP;
+
+var functions = new Functions();
+var list = functions.GenerateAbstrctobjects();
+//var list = functions.LoadFromFile("text.xml");
+//functions.SaveFile("text.xml", list);
+
+functions.PrintList(list);
+
+var result = list.Where(x => x.AbstractInt > 0).GroupBy(x => x.AbstractInt).Select(g => new Result { Id = g.Key, Count = g.Count() }).ToList();
+
+foreach (var item in result)
+{
+    Console.WriteLine($"{item.Id} - количество {item.Count}");
+}
